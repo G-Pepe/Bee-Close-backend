6a2a3f777feef590b629c9e9368264c221b445e6
Aiham <aiham.rabee@gmail.com>
2021-04-08 11:14:49 +0200

    added7

--- Models/userSchema.js
+++ Models/userSchema.js
@@ -3,7 +3,7 @@ const { encrypt } = require("../lib/encryption");
 
 // creating the schema for the user
 const UserSchema = new mongoose.Schema({
-  username: { type: String, required: true },
+  
   firstName: { type: String, required: true },
   lastName: { type: String, required: true },
   password: { type: String, required: true },
--- Routes/usersRoute.js
+++ Routes/usersRoute.js
@@ -10,52 +10,54 @@ const auth= require("../Middlewares/authJwt")
 
 router.post("/signup", async (req, res, next) => {
   try {
-    await User.findOne({
-      email: req.body.email,
-    }).then((user) => {
-      if (user) {
-        return res.status(400).send({ message: "user already registered" });
-      } else {
-        const newUser = new User(req.body);
-        newUser.save();
-        res.status(200).send({ message: "User registered successfully" });
-        
-      }
-    });
-  } catch (err) {
-    next(err);
+  await User.findOne({
+  email: req.body.email,
+  }).then((user) => {
+  if (user) {
+  return res.status(400).send({ message: "user already registered" });
+  } else {
+  const newUser = new User(req.body);
+  
+  newUser.save();
+  res.status(200).send({ message: "User registered successfully" });
   }
-});
-
-
-router.post("/login",(req, res, next) => {
-  try {
-    User.findOne({
-      email: req.body.email,
-    }).then((user) => {
-      if (!user) {
-        return res.status(401).send({ message: "incorrect email or password" });
-      } else {
-        /* console.log(req.body);
-        console.log(user); */
-        let match = bcrypt.compareSync(req.body.password, user.password);
-        if (!match) {
-          return res
-            .status(401)
-            .send({ message: "incorrect password or email" });
-        } else {
-          let token = jwt.sign({ _id: user._id, email: user.email }, process.env.SECRET)
-          res.status(200)
-          .header("x-auth", token)
-          .send({ message: "login successful", success: true, user: user, token:token })
-
-          
-        }
-      }
-    });
+  });
   } catch (err) {
-    next(err);
+  next(err);
   }
-});
+  }); 
 
+
+  router.post("/login", async (req, res, next) => {
+    try {
+    const user = await User.findOne({
+    email: req.body.email,
+    });
+    
+    if (!user) {
+    return res.status(401).send({ message: "incorrect email or password" });
+    }
+    
+    const match = bcrypt.compareSync(req.body.password, user.password);
+    if (!match) {
+    return res.status(401).send({ message: "incorrect password or email" });
+    }
+    
+    const token = jwt.sign(
+    { _id: user._id, email: user.email },
+    process.env.SECRET
+    );
+    
+    console.log(token);
+    
+    res.status(200).header("x-auth", token).send({
+    message: "login successful",
+    success: true,
+    user: user,
+    token: token,
+    });
+    } catch (err) {
+    next(err);
+    }
+    }); 
 module.exports = router;
