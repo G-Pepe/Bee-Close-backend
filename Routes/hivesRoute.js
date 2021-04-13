b66518ad170f5de5c377c63d67621287ab30cdcc
Aiham <aiham.rabee@gmail.com>
2021-04-13 11:55:32 +0200

    added 11

--- Routes/hivesRoute.js
+++ Routes/hivesRoute.js
@@ -1,44 +1,48 @@
 const express = require("express");
 const router = express.Router();
 const Hives = require("../Models/hiveSchema");
-const auth = require('../Middlewares/authJwt')
+const Users = require("../Models/userSchema");
+const auth = require("../Middlewares/authJwt");
 // creating/joining an hive
-router.post("/joinhive/:userId",auth, async (req, res, next) => {
-  const { address } = req.body; // we can remove this line
+router.post("/joinhive/:userId", auth, async (req, res, next) => {
   const { userId } = req.params;
-
   // TODO normalise / sanitise street
   try {
-    const existingHive = await Hives.findOne({
-      "address.street": req.body.address.street,
-      "address.houseNumber": req.body.address.houseNumber,
-      "address.postalCode": req.body.address.postalCode,
+    const user = await Users.findOne({
+      _id: userId,
     });
-
+    const existingHive = await Hives.findOne({
+      "address.street": user.address.street,
+      "address.houseNumber": user.address.houseNumber,
+      "address.postalCode": user.address.postalCode,
+    });
     if (existingHive) {
       await existingHive.update({ $push: { users: userId } });
-
       console.log("existing hive", existingHive);
-
-      return res.status(200).json("User successfully added to his hive");
+      return res
+        .status(200)
+        .send({
+          message: "User successfully added to his hive",
+          success: true,
+        });
     }
-
     await Hives.create({
-      address: address,
+      address: user.address,
       secretKey: "randomSecretKey",
       users: [userId],
       posts: [],
     });
-
     res
       .status(200)
-      .send({ message: "Congratulation: you have created your hive" });
+      .send({
+        message: "Congratulation: you have created your hive",
+        success: true,
+      });
   } catch (err) {
     next(err);
   }
 });
-
-router.get("/findhive/:hiveId",auth, async (req, res, next) => {
+router.get("/findhive/:hiveId", auth, async (req, res, next) => {
   const { hiveId } = req.params;
   try {
     const singleHive = await Hives.findById(hiveId)
@@ -47,7 +51,6 @@ router.get("/findhive/:hiveId",auth, async (req, res, next) => {
         "-_id -__v -password -email -lastName -city -streetNumber -street -postCode -gender"
       )
       .populate("posts", "-id -timestamp");
-
     if (singleHive) {
       res.status(200).send({ success: true, singleHive });
     }
@@ -55,5 +58,4 @@ router.get("/findhive/:hiveId",auth, async (req, res, next) => {
     next(err);
   }
 });
-
 module.exports = router;
