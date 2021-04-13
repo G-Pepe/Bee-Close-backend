2b3a4223893ee714033c48e2a1e044b5f47bff3a
Franco Speziali <franco.speziali@digitalcareerinstitute.org>
2021-04-13 15:18:35 +0200

    Updated auth stack

--- Middlewares/authJwt.js
+++ Middlewares/authJwt.js
@@ -4,22 +4,25 @@ const User = require("../Models/userSchema");
 dotenv.config()
 
 const auth = (req, res, next) => {
-  const authHeader = req.headers["Authorization"];
+  const authHeader = req.get("Authorization");
+
     if (authHeader) {
-    const token = authHeader.split(" ")[1];
+      const token = authHeader.split(" ")[1];
 
-    if (token==null) {
-      return res.status(403).send({ message: "No token provided!" });
-    }
-
-    jwt.verify(token, process.env.SECRET, (err, decoded) => {
-      if (err) {
-        return res.status(401).send({ message: "Unauthorized!" });
+      if (token === null) {
+        return res.status(403).send({ message: "No token provided!" });
       }
-      req.userId = decoded.userId;
-      req.hiveId = decoded.hiveId;
+
+      jwt.verify(token, process.env.SECRET, (err, decoded) => {
+        if (err) {
+          return res.status(401).send({ message: "Unauthorized!" });
+        }
+        req.userId = decoded.userId;
+        req.hiveId = decoded.hiveId;
+        next();
+      });
+  } else {
       next();
-    });
   }
 };
 
